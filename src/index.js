9f6dee4e42cc1853ff51167eb038f02179e7206c
lautarohelp <lautarotoledo391@gmail.com>
2023-09-12 16:21:44 -0600

    notificaciones

--- src/index.js
+++ src/index.js
@@ -19,10 +19,16 @@ const LEARNMore3 = document.querySelector(".LEARNMore3");
 const ReturnNed = document.querySelector(".ReturnNed");
 const ReturnNed2 = document.querySelector(".ReturnNed2");
 const ReturnNed3 = document.querySelector(".ReturnNed3");
+const notificacionesOff = document.querySelector(".imgNoti")
+const notificacionesOm = document.querySelector(".imgNotic")
+const notification = document.querySelector(".notification")
+const divContainer = document.getElementById('NotifiContainer');
 
 const sectionsGenerales = [sectionBasic,sectionProfessional,sectionMaster]
 const numberAnual = [numberAnual1,numberAnual2,numberAnual3];
-const numberMensual =[numberMensual1,numberMensual2,numberMensual3];
+const numberMensual = [numberMensual1,numberMensual2,numberMensual3];
+const personasRegistradas = []
+
 
 LEARNMore.addEventListener('click',informationGlod);
 LEARNMore2.addEventListener('click',informationGlod2);
@@ -32,32 +38,93 @@ ReturnNed2.addEventListener('click',returnNAD2);
 ReturnNed3.addEventListener('click',returnNAD3);
 containerBoll.addEventListener('click',openAnual);
 containerBoll.addEventListener('click', offButton);
+notificacionesOff.addEventListener('click',seenNotifications)
+notificacionesOff.addEventListener('click' , offNoti);
+
+
+
+
+function openNoti() {
+    if (personasRegistradas.length == 0) {
+        notificacionesOff.classList.add('inactive');
+        notificacionesOm.classList.remove('inactive');
+        notification.classList.add('inactive');
+    }else if(personasRegistradas.length >= 0){
+        notificacionesOff.classList.remove('inactive');
+        notificacionesOm.classList.add('inactive');
+    }
+}
+
+function offNoti() {
+    if (notification.classList.contains('inactive')) {
+        notification.classList.remove('inactive');
+    }else if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+        
+    }
+}
 
 function informationGlod() {
     information.classList.remove('inactive');
     sectionsGenerales[0].classList.add('sectionInfo');
+    personasRegistradas.push('A BASIC person has been registered')
+    openNoti();
 }
 function informationGlod2() {
     information2.classList.remove('inactive');
     sectionsGenerales[1].classList.add('sectionInfo');
+    personasRegistradas.push('A person has been registered as a PROFESSIONAL')
+    openNoti();
+
+    if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+    }
 }
 
 function informationGlod3() {
     information3.classList.remove('inactive');
     sectionsGenerales[2].classList.add('sectionInfo');
+    personasRegistradas.push('A person has been registered as a MASTER')
+    openNoti();
+
+    if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+        
+    }
+    
 }
 function returnNAD () {
-        information.classList.add('inactive')
-        sectionsGenerales[0].classList.remove('sectionInfo');
+    information.classList.add('inactive')
+    sectionsGenerales[0].classList.remove('sectionInfo');
+    personasRegistradas.pop('A BASIC person has been registered');
+    openNoti();
+
+    if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+    }
 }
 
 function returnNAD2 () {
     information2.classList.add('inactive')
     sectionsGenerales[1].classList.remove('sectionInfo');
+    personasRegistradas.pop('A person has been registered as a PROFESSIONAL')
+    openNoti();
+
+    if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+        
+    }
 }
 function returnNAD3() {
     information3.classList.add('inactive')
     sectionsGenerales[2].classList.remove('sectionInfo');
+    personasRegistradas.pop('A person has been registered as a MASTER')
+    openNoti();
+
+    if(!notification.classList.contains('inactive')){
+        notification.classList.add('inactive');
+        
+    }
 }
 
 function offButton() {
@@ -76,9 +143,21 @@ function openAnual() {
     if (hay) {
         numberAnual.forEach(item => item.classList.remove("inactive"))
         numberMensual.forEach(item => item.classList.add("inactive"))
-        console.log('anual');
     }else if (hey){
         numberAnual.forEach(item => item.classList.add("inactive"))
         numberMensual.forEach(item => item.classList.remove("inactive"))
     }
 }
+
+
+function seenNotifications() {
+    const divContainer = document.getElementById('NotifiContainer');
+    divContainer.innerHTML = '';
+    personasRegistradas.map(item => {
+        const i = document.createElement('div')
+        i.innerText = item
+        divContainer.insertAdjacentElement('beforeend', i)
+    })
+}
+
+
